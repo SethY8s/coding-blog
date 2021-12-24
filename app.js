78f97e87669c03838bff03ce603443cc6e78c3b9
Seth8656 <89519946+Seth8656@users.noreply.github.com>
2021-12-23 21:17:00 -0500

    added the form and basic structure. also getting the submit button working.

---
+++ app.js
@@ -0,0 +1,15 @@
+let blog = document.querySelector('.blog-container')
+
+let date = document.getElementById('date');
+let subject = document.getElementById('subject');
+let blogMess = document.getElementById('blog');
+
+
+blog.addEventListener('submit', function (e) {
+        e.preventDefault();
+        console.log(subject.value)
+        
+
+    })
+// console.log(date.value)
+console.log("penis")
