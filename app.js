e72b202d1b6c3c471ad0ef61b3c90cda07f1d2b7
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2021-12-27 20:46:52 -0500

    added Node and started server

--- app.js
+++ app.js
@@ -1,44 +1,71 @@
-let blog = document.querySelector('.blog-container')
+let fs = require('fs');
+const express = require('express');
+const app = express();
 
-let date = document.getElementById('date');
-let subject = document.getElementById('subject');
-let blogMess = document.getElementById('blog');
+const PORT = 5000;
+
+// middleware
+app.use(express.static('public'));
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/public/index.html');
+
+})
+
+app.listen(PORT, ()=>{
+    console.log('Server Running on 5000')
+})
 
 
-blog.addEventListener('submit', function (e) {
-        e.preventDefault();
-        const dateValue = date.value;
-        const subjectValue = subject.value;
-        const blogMessValue = blogMess.value;
 
-        // header for date
+// let blog = document.querySelector('.blog-container')
 
-        const dateHeader = document.createElement('h2');
-        dateHeader.innerText = dateValue;
-        document.body.appendChild(dateHeader);
+// let date = document.getElementById('date');
+// let subject = document.getElementById('subject');
+// let blogMess = document.getElementById('blog');
 
-        // header for Subject
 
-        const subjectHeader = document.createElement('h3');
-        subjectHeader.innerText = subjectValue;
-        document.body.appendChild(subjectHeader);
+// blog.addEventListener('submit', function (e) {
+//         e.preventDefault();
+//         const dateValue = date.value;
+//         const subjectValue = subject.value;
+//         const blogMessValue = blogMess.value;
 
-        // paragraph for blog message
+//         // header for date
 
-        const blogMessPar = document.createElement('p');
-        blogMessPar.innerText = blogMessValue;
-        document.body.appendChild(blogMessPar);
+//         const dateHeader = document.createElement('h2');
+//         dateHeader.innerText = `${dateValue}`;
+//         document.body.appendChild(dateHeader);
+//         localStorage.setItem(dateHeader, dateValue)
 
-        const deleteButton =  document.createElement('button')
-        deleteButton.innerText = 'delete post';
-        document.body.appendChild(deleteButton);
+//         // header for Subject
 
-        deleteButton.addEventListener('click', function () {
-            dateHeader.remove();
-            subjectHeader.remove();
-            blogMessPar.remove();
-            deleteButton.remove();
-        })
+//         const subjectHeader = document.createElement('h3');
+//         subjectHeader.innerText = subjectValue;
+//         document.body.appendChild(subjectHeader);
+//         localStorage.setItem(subjectHeader, subjectValue)
 
-    })
-// console.log(date.value)
+//         // paragraph for blog message
+
+//         const blogMessPar = document.createElement('p');
+//         blogMessPar.innerText = blogMessValue;
+//         document.body.appendChild(blogMessPar);
+//         localStorage.setItem(blogMessPar,blogMessValue)
+
+//         // button w/ delete function
+
+//         const deleteButton =  document.createElement('button')
+//         deleteButton.innerText = 'delete post';
+//         document.body.appendChild(deleteButton);
+
+//         deleteButton.addEventListener('click', function () {
+//             dateHeader.remove();
+//             subjectHeader.remove();
+//             blogMessPar.remove();
+//             deleteButton.remove();
+//         })
+
+        
+
+//     })
+// // console.log(date.value)
